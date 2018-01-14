77cc3b2f0e8f8d6ce76e7e0ca6bc77d7b307fe4f
Rens Verhage <rens.verhage@topicus.nl>
2018-01-14 19:51:56 +0100

    Added Webwork action for project settings

---
+++ src/main/java/nl/topicus/overheid/build/status/webwork/BuildStatusProjectSettingsAction.java
@@ -0,0 +1,14 @@
+package nl.topicus.overheid.build.status.webwork;
+
+import com.atlassian.jira.web.action.ProjectActionSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BuildStatusProjectSettingsAction extends ProjectActionSupport {
+    private static final Logger log = LoggerFactory.getLogger(BuildStatusProjectSettingsAction.class);
+
+    @Override
+    protected String doExecute() throws Exception {
+        return super.doExecute();
+    }
+}
---
+++ src/test/java/ut/nl/topicus/overheid/build/status/webwork/BuildStatusProjectSettingsActionTest.java
@@ -0,0 +1,34 @@
+package ut.nl.topicus.overheid.build.status.webwork;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import nl.topicus.overheid.build.status.webwork.BuildStatusProjectSettingsAction;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @since 3.5
+ */
+public class BuildStatusProjectSettingsActionTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test(expected=Exception.class)
+    public void testSomething() throws Exception {
+
+        //BuildStatusProjectSettingsAction testClass = new BuildStatusProjectSettingsAction();
+
+        throw new Exception("BuildStatusProjectSettingsAction has no tests!");
+
+    }
+
+}
